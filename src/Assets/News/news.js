46b7dc3e3ee321421f96477ea75ef5e455e85550
Jose Tirado <jose_enrique_29@hotmail.com>
2020-03-30 17:09:32 -0400

    fresh start

--- src/Assets/News/news.js
+++
@@ -1,7 +0,0 @@
-export default {
-    news1: "Inicia la semana de atención medica a los ciudadanos de Nueva Casarapa.",
-    news2: "Culminamos la limpieza de la laguna de Nueva Casarapa, Click en leer para ver las fotos.",
-    news3: "Luchando contra el SITSA, ciudadanos merecen mejor transporte!.",
-    news4: "Así son las practicas del grupo de scouts Alianza.",
-    news5: "Cuidado con estas personas, pueden jugarle malos trucos!.",
-}
